9efaa1646aa974c4577f412cc7127850f60a0368
Degensoft-vlad <140105174+Degensoft-vlad@users.noreply.github.com>
2025-02-26 00:20:10 +0200

    Add Chains to Bridge

--- src/model/gaszip/constants.py
+++ src/model/gaszip/constants.py
@@ -2,12 +2,16 @@ GASZIP_RPCS = {
     "Arbitrum": "https://rpc.ankr.com/arbitrum",
     "Optimism": "https://rpc.ankr.com/optimism",
     "Base": "https://rpc.ankr.com/base",
+    "ZkSync": "https://rpc.ankr.com/zksync_era",
+    "Scroll": "https://rpc.ankr.com/scroll"
 }
 
 GASZIP_EXPLORERS = {
     "Arbitrum": "https://arbiscan.io/tx/0x",
     "Optimism": "https://optimistic.etherscan.io/tx/0x",
     "Base": "https://basescan.org/tx/0x",
+    "Scroll": "https://scrollscan.com/tx/0x",
+    "ZkSync": "https://explorer.zksync.io/tx/",
 }   
 
 REFUEL_ADDRESS = "0x391E7C679d29bD940d63be94AD22A25d25b5A604"
